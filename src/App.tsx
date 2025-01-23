a8007220dfbe920b0ddd105caaa85cf09bc36847
peuransiseu <90952718+peuransiseu@users.noreply.github.com>
2025-01-23 19:48:15 +0800

    remove unread values

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import NavBar from "./components/NavBar";
