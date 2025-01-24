7da3e7ae09d79cdf7518ec2f25af763ce46ebe8c
peuransiseu <90952718+peuransiseu@users.noreply.github.com>
2025-01-24 14:35:35 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import NavBar from "./components/NavBar";
 import Intro from "./components/Intro";
 import OrgsExp from "./components/OrgsExp";
 import About from "./components/About";
-import Projects from "./components/Projects";
+// import Projects from "./components/Projects";
+import MyCarousel from "./components/MyCarousel";
 
 const AnimatedRoutes: React.FC = () => {
   const location = useLocation(); // Detects current route
@@ -15,7 +16,7 @@ const AnimatedRoutes: React.FC = () => {
         <Route path="/" element={<Intro />} />
         <Route path="/about" element={<About />} />
         <Route path="/orgsexp" element={<OrgsExp />} />
-        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects" element={<MyCarousel />} />
       </Routes>
     </AnimatePresence>
   );
@@ -24,8 +25,10 @@ const AnimatedRoutes: React.FC = () => {
 const App: React.FC = () => {
   return (
     <>
+      {/* <MyCarousel /> */}
       <div className="main-container">
         <NavBar />
+        {/* <MyCarousel /> */}
         <AnimatedRoutes />
       </div>
     </>
