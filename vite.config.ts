0086ec600fa7bd4a4fd8e78c00b80a44e5c5cd7b
peuransiseu <90952718+peuransiseu@users.noreply.github.com>
2025-06-25 12:47:38 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my-portfolio-app/"
+  base: "https://peuransiseu.github.io"
 })
